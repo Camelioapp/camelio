6ca9e0c4c5dfe278222b4413ac056c856e7cea62
Camelioapp <info@camelio.app>
2026-05-27 21:36:03 -0400

    2026-05-27-30

---
+++ scripts/generateProfilManifest.js
@@ -0,0 +1,6 @@
+const folders = {
+  fille: path.join(profilDir, "Fille"),
+  garcon: path.join(profilDir, "Garcon"),
+  mere: path.join(profilDir, "Parent", "Mere"),
+  pere: path.join(profilDir, "Parent", "Pere"),
+};
